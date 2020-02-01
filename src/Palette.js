cff05bc395d34bb93a39a0d99d59bbe7488113c0
Florent VICENTE <vicentegroupe@gmail.com>
2020-02-01 22:10:57 +0100

    adding navbar and footer

--- src/Palette.js
+++ src/Palette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ColorBox from './ColorBox'
 import "./Palette.css"
 import Navbar from './Navbar'
+import PaletteFooter from './PaletteFooter'
 
 
 
@@ -39,14 +40,12 @@ class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
+          showingAllColors={true}
         />
         <div className="Palette-colors">
           {colorBoxes}
         </div>
-        <footer className="Palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     )
   }
