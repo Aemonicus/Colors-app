ac3fb3f4fd1a2a5dad50e47d6cf878a2fdbbb52f
Florent VICENTE <vicentegroupe@gmail.com>
2020-02-08 15:50:54 +0100

    adding color box delete

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -144,9 +144,16 @@ class NewPaletteForm extends Component {
     this.props.history.push("/")
   }
 
+  removeColor(colorName) {
+    this.setState({
+      colors: this.state.colors.filter(color => color.name !== colorName)
+    })
+  }
+
   render() {
     const { classes } = this.props
     const { open } = this.state
+
     return (
       <div className="classes.root">
         <CssBaseline />
@@ -247,7 +254,12 @@ class NewPaletteForm extends Component {
           <div className={classes.drawerHeader} />
 
           {this.state.colors.map(color => (
-            <DraggableColorBox color={color.color} name={color.name} />
+            <DraggableColorBox
+              key={color.name}
+              color={color.color}
+              name={color.name}
+              handleClick={() => this.removeColor(color.name)}
+            />
           ))}
 
         </main>
