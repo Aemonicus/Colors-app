01be1f0e7fdc893185ca942b2c4fa22586e54c11
Florent VICENTE <vicentegroupe@gmail.com>
2020-02-08 11:09:32 +0100

    saving new palette

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -90,6 +90,7 @@ class NewPaletteForm extends Component {
     this.updateCurrentColor = this.updateCurrentColor.bind(this)
     this.addNewColor = this.addNewColor.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
@@ -126,6 +127,17 @@ class NewPaletteForm extends Component {
     this.setState({ newName: event.target.value })
   }
 
+  handleSubmit() {
+    let newName = "New test"
+    const newPalette = {
+      paletteName: newName,
+      id: newName.toLocaleLowerCase().replace(/ /g, "-"),
+      colors: this.state.colors
+    }
+    this.props.savePalette(newPalette)
+    this.props.history.push("/")
+  }
+
   render() {
     const { classes } = this.props
     const { open } = this.state
@@ -150,6 +162,12 @@ class NewPaletteForm extends Component {
             <Typography variant="h6" color="inherit" noWrap>
               Blabla
           </Typography>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={this.handleSubmit}>
+              Save Palette
+              </Button>
           </Toolbar>
         </AppBar>
         <Drawer
