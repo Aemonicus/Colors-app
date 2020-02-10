8ed2268d667519e832bc71e7fd6934585bd77506
Florent VICENTE <vicentegroupe@gmail.com>
2020-02-10 10:25:46 +0100

    optimize with purecomponent

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -28,6 +28,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this)
     this.closeDialog = this.closeDialog.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
+    this.goToPalette = this.goToPalette.bind(this)
   }
 
   openDialog(id) {
@@ -65,7 +66,7 @@ class PaletteList extends Component {
                 timeout={500}
               >
                 <MiniPalette {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
+                  goToPalette={this.goToPalette}
                   // handleDelete={deletePalette}
                   openDialog={this.openDialog}
                   key={palette.id}
